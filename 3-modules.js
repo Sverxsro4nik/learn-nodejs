2dca657d16c0626828291b8dfb169377cd7bd5c7
Sverxsro4nik <sverxsro4nik@gmail.com>
2024-03-08 22:54:49 +0500

    learn about modules in nodejs

---
+++ 3-modules.js
@@ -0,0 +1,8 @@
+// Modules
+const sayHi = require('./5-utils');
+const { john, peter } = require('./4-names');
+const data = require('./6-alternative-flavor');
+require('./7-mind-granade');
+sayHi('susan');
+sayHi(john);
+sayHi(peter);
