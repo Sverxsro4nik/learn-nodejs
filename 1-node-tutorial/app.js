53bfa9525637812ec789ce084b4726ae96eb17ee
Sverxsro4nik <Sverxsro4nik@gmail.com>
2024-04-17 15:37:55 +0500

    about stream

--- 1-node-tutorial/app.js
+++ 1-node-tutorial/app.js
@@ -1,35 +1,9 @@
-const { readFile, writeFile } = require('fs');
-const util = require('util');
+const http = require('http');
+const fs = require('fs');
 
-const readFilePromise = util.promisify(readFile);
-const writeFilePromise = util.promisify(writeFile);
-
-const getText = path => {
-	return new Promise((resolve, rejects) => {
-		readFile(path, 'utf-8', (err, result) => {
-			if (err) {
-				rejects(err);
-			} else {
-				resolve(result);
-			}
-		});
-	});
-};
-
-getText('./content/first.txt')
-	.then(result => console.log(result))
-	.catch(err => console.log(err));
-
-const start = async () => {
-	try {
-		const first = await readFilePromise('./content/first.txt', 'utf-8');
-		const second = await readFilePromise('./content/second.txt', 'utf-8');
-		await writeFilePromise(
-			'./content/result.txt-mind-grande',
-			`THIS IS AWESOME : ${first}, ${second}`
-		);
-	} catch (error) {
-		console.log(error);
-	}
-};
-start();
+http
+	.createServer(function (req, res) {
+		const text = fs.readFileSync('./content/big.txt', 'utf8');
+		res.end(text);
+	})
+	.listen(5000);
