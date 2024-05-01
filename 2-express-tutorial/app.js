b6d0bea350192fc8ee5595740538e68fe4fca7d9
Sverxsro4nik <Sverxsro4nik@gmail.com>
2024-05-01 23:06:49 +0500

    learn express methods

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,24 +1,72 @@
 const express = require('express');
 const path = require('path');
-const { products } = require('./data');
-const logger = require('./logger');
-const authorize = require('./authorize');
-const morgan = require('morgan');
+const { people } = require('./data');
 
 const app = express();
 
-// req => middleware => res
-// app.use([logger, authorize]);
-// app.use(express.static('./public'));
-app.use(morgan('tiny'));
+// Setup static and middleware
+app.use(express.static('./methods-public'));
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+// parse json
+app.use(express.json());
 
-app.get('/', (req, res) => {
-	res.send('<h1>Home Page</h1>');
+app.get('/api/people', (req, res) => {
+	res.status(200).json({ success: true, data: people });
 });
 
-app.get('/about', (req, res) => {
-	res.send('<h1>About Page</h1>');
+app.post('/login', (req, res) => {
+	const { name } = req.body;
+	if (!person) {
+		return res
+			.status(401)
+			.json({ success: false, msg: `No person with name ${name}` });
+	}
+	return res.status(200).json({ success: true, person: name });
 });
+
+app.post('/api/people', (req, res) => {
+	const { name } = req.body;
+	if (!name) {
+		return res
+			.status(400)
+			.json({ success: false, msg: 'Please provide name value' });
+	}
+	res.status(201).json({ success: true, person: name });
+});
+
+app.put('/api/people/:id', (req, res) => {
+	const { id } = req.params;
+	const { name } = req.body;
+	const person = people.find(person => person.id === Number(id));
+	if (!person) {
+		return res
+			.status(404)
+			.json({ success: false, msg: `No person with id ${id}` });
+	}
+
+	const newPeople = people.map(person => {
+		if (person.id === Number(id)) {
+			person.name = name;
+		}
+		return person;
+	});
+	res.status(200).json({ success: true, data: newPeople });
+});
+
+app.delete('/api/people/:id', (req, res) => {
+	const person = people.find(person => person.id === Number(req.params.id));
+	if (!person) {
+		return res
+			.status(404)
+			.json({ success: false, msg: `No person with id ${req.params.id}` });
+	}
+	const newPeople = people.filter(
+		person => person.id !== Number(req.params.id)
+	);
+	return res.status(200).json({ success: true, data: newPeople });
+});
+
 app.listen(5000, () => {
 	console.log('Server is listening on port 5000...');
 });
